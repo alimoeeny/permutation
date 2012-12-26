b1db1a9172b6fd4adac34b3ccd798daf27307ca5
Ingo Oeser <nightlyone@googlemail.com>
2012-12-26 17:14:41 +0100

    more and better testing

--- permute_test.go
+++ permute_test.go
@@ -10,8 +10,8 @@ type byteSlice []byte
 func (p byteSlice) Len() int              { return len(p) }
 func (p byteSlice) Less(i, j int) bool    { return p[i] < p[j] }
 func (p byteSlice) Swap(i, j int)         { p[i], p[j] = p[j], p[i] }
-func (p byteSlice) Equal(q Sequence) bool { return bytes.Compare(p, q.(byteSlice)) == 0 }
-func (p byteSlice) Copy() Sequence        { return p[:] }
+func (p byteSlice) Equal(q Sequence) bool { return bytes.Equal(p, q.(byteSlice)) }
+func (p byteSlice) Copy() Sequence        { q := make(byteSlice, len(p), len(p)); copy(q, p); return q }
 
 type permTest struct {
 	seq   []byte
@@ -32,7 +32,7 @@ var permsPositive = [...]permTest{
 	},
 }
 
-func TestNext(t *testing.T) {
+func TestIteration(t *testing.T) {
 	for test, a := range permsPositive {
 		p := New(byteSlice(a.seq))
 		for i, ok, seq := 0, true, p.Current().(byteSlice); ok; i, ok, seq = i+1, p.Next(), p.Current().(byteSlice) {
@@ -40,6 +40,26 @@ func TestNext(t *testing.T) {
 				t.Errorf("%d: seq=%s, want=%s, got=%s", test, a.seq, a.perms[i], seq)
 			} else {
 				t.Logf("%d: seq=%s, want=%s, got=%s", test, a.seq, a.perms[i], seq)
+			}
+		}
+	}
+}
+
+func TestCompleteness(t *testing.T) {
+	for test, a := range permsPositive {
+		seen := map[string]bool{}
+		for i := 0; i < len(a.perms); i++ {
+			seen[string(a.perms[i])] = false
+		}
+		p := New(byteSlice(a.seq))
+		for ok, seq := true, p.Current().(byteSlice); ok; ok, seq = p.Next(), p.Current().(byteSlice) {
+			seen[string(seq)] = true
+		}
+		for seq, ok := range seen {
+			if ok {
+				t.Logf("%d: seen=%s", test, seq)
+			} else {
+				t.Errorf("%d: missing=%s", test, seq)
 			}
 		}
 	}
